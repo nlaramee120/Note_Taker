bc6ad2fd8b8b43d7cf0756ace0fc4aa4212d20e5
nlaramee120 <nlaramee120@aol.com>
2021-10-12 18:43:25 -0500

    Added a writetofile function and edited the readme.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const database = require('./db/db.json')
+const uuid = require("./helpers/uuid.js")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,9 +22,45 @@ app.get('*', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(database);
-})
+});
 
+// Pulled straight from activity 21 to write to the file
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
 
+// Pulled straight from activity 21 to read from file and append to page
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+  
+  app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
 
 
